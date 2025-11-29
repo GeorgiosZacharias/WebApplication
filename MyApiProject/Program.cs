675d66510f57be0ca113a22a1699fa480b0d0aff
George Zacharias <zachariasg7@hotmail.gr>
2025-11-29 14:41:25 +0200

    get method

--- MyApiProject/Program.cs
+++ MyApiProject/Program.cs
@@ -1,8 +1,15 @@
+using WebApplication.Models;
 var builder = WebApplication.CreateBuilder(args);
 
 var app = builder.Build();
-
+var items = new List<Item>();
 // Basic routes
+app.MapGet("/items", () => items);
+app.MapGet("/items/{id}", (int id) =>
+{
+    var item = items.FirstOrDefault(i => i.Id == id);
+    return item is not null ? Results.Ok(item) : Results.NotFound();
+});
 
 app.MapGet("/", () => "Welcome to the Simple Web API!");
 
