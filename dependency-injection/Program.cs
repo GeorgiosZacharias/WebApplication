fb8b778a840dab0d897367b83e96742cdea36557
George Zacharias <zachariasg7@hotmail.gr>
2025-12-03 20:36:20 +0200

    middleware implementation

--- dependency-injection/Program.cs
+++ dependency-injection/Program.cs
@@ -1,6 +1,20 @@
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddSingleton<IMyService, MyService>();
 var app = builder.Build();
+
+app.Use(async (context, next) =>
+{
+    var myService = context.RequestServices.GetRequiredService<IMyService>();
+    myService.LogCreation("first middleware");
+    await next.Invoke();
+});
+app.Use(async (context, next) =>
+{
+    var myService = context.RequestServices.GetRequiredService<IMyService>();
+    myService.LogCreation("second middleware");
+    await next.Invoke();
+});
+
 app.MapGet("/", (IMyService myService) =>
 {
     myService.LogCreation("Root");
