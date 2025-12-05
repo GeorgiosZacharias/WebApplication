7f350c529e971429582573c5fa9d988e5f09320b
George Zacharias <zachariasg7@hotmail.gr>
2025-12-05 19:07:31 +0200

    small fix

--- dependency-injection/Program.cs
+++ dependency-injection/Program.cs
@@ -23,20 +23,5 @@ app.MapGet("/", (IMyService myService) =>
 app.Run();
 
 
-public interface IMyService
-{
-    void LogCreation(string message);
-}
 
-public class MyService : IMyService
-{
-    private readonly int _serviceId;
-    public MyService()
-    {
-        _serviceId = new Random().Next(100000, 999999);
-    }
-    public void LogCreation(string message)
-    {
-        Console.WriteLine($"{message} - Service ID: {_serviceId}");
-    }
-}
+
