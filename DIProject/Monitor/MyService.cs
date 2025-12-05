26bf01a74839e1970c9cf324d0e9126d222b78ed
George Zacharias <zachariasg7@hotmail.gr>
2025-12-05 19:49:30 +0200

    added service into program.cs

---
+++ DIProject/Monitor/MyService.cs
@@ -0,0 +1,14 @@
+using System;
+
+public class MyService : IMyService
+{
+    private readonly int _serviceId;
+    public MyService()
+    {
+        _serviceId = new Random().Next(100000, 999999);
+    }
+    public void LogCreation(string message)
+    {
+        Console.WriteLine($"{message} - Service ID:{_serviceId}");
+    }
+}
